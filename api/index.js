fbff04f6a1dd9fa2b75dea3067d7904f3dcadd4b
jonahlipsky <jonahlipsky@gmail.com>
2020-06-09 16:53:47 -0400

    Run first migration

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const api = express();
 const port = 3000;
+const { Client } = require('pg');
+const client = new Client();
+
+client.connect();
+client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+  console.log(err ? err.stack : res.rows[0].message) // Hello World!
+  client.end();
+});
 
 api.get('/', (req, res) => res.send('hello world'));
 
