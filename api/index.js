3a62c21e99ae218e6f1e2eaf8bba033e5cabb1a0
jonahlipsky <jonahlipsky@gmail.com>
2020-06-09 17:22:27 -0400

    Add documentation and add route

--- api/index.js
+++ api/index.js
@@ -12,4 +12,20 @@ client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
 
 api.get('/', (req, res) => res.send('hello world'));
 
+api.get('/todos', (req, res) => {
+  let client2 = new Client({ database: 'devDB'});
+  client2.connect()
+  let response;
+  client2.query('SELECT * FROM todos', (err, res) => {
+    if (err) {
+      console.log('Failed to query todos')
+    } else {
+      console.log(res.rows[0])
+      console.log(res.rows)
+      response = res.row[0]
+    }
+  })
+  res.send(response);
+})
+
 api.listen(port, () => console.log(`Listening on port ${port}`));
