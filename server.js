bd3b599621ab33caf3098571b6027bb0b00d79e9
jonahlipsky <jonahlipsky@gmail.com>
2020-07-03 10:44:27 -0400

    Remove ssl

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ require('dotenv').config()
 const { Client } = require('pg');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
 });
 
 client.connect();
