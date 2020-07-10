48dca877304c4840030301133005ca8e14aba0c8
jonahlipsky <jonahlipsky@gmail.com>
2020-07-10 10:11:11 -0400

    Add basic resolver for todos

--- server.js
+++ server.js
@@ -12,44 +12,55 @@ const { graphql, buildSchema } = require('graphql');
 const schema = buildSchema(`
   type Query {
     hello: String
+    todos: [Todo]
+  },
+  type Todo {
+    name: String!
   }
 `);
+
+const root = {
+  hello: () => {
+    return 'Hello World!';
+  },
+  todos: () => {
+    return [{name: "hello"}, {name:"world"}]
+  }
+};
 
 api.use(cors())
 api.use(
   '/graphql',
   graphqlHTTP({
     schema: schema,
+    rootValue: root,
     graphiql: true,
   }),
 );
 
-
-
-// client.connect();
-// client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-//   console.log(err ? err.stack : res.rows[0].message) // Hello World!
-//   client.end();
-// });
+async function getAllTodos () {
+  let client2 = new Client({ database: process.env.POSTGRES_NAME});
+  client2.connect()
+  let response;
+  client2.query('SELECT * FROM todos', async (err, res) => {
+    if (err) {
+      console.log('Failed to query todos')
+      routeRes.send('Failure')
+    } else {
+      console.log(res.rows[0])
+      console.log(res.rows)
+      response = res.rows
+      console.log('sending response')
+      return response
+    }
+  })
+}
 
 // api.get('/', (req, res) => res.send('hello world'));
 
 // api.get('/todos', (req, routeRes) => {
-//   let client2 = new Client({ database: process.env.POSTGRES_NAME});
-//   client2.connect()
-//   let response;
-//   client2.query('SELECT * FROM todos', async (err, res) => {
-//     if (err) {
-//       console.log('Failed to query todos')
-//       routeRes.send('Failure')
-//     } else {
-//       console.log(res.rows[0])
-//       console.log(res.rows)
-//       response = res.rows
-//       console.log('sending response')
-//       routeRes.send(response);
-//     }
-//   })
+//   response = await getAllTodos()
+//   routeRes.send(response)
 // })
 
 api.listen(port, () => console.log(`Listening on port ${port}`));
