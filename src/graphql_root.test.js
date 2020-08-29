028a365e24e682ad12b456746b92d2c181b3c4a5
jonahlipsky <jonahlipsky@gmail.com>
2020-08-29 16:32:35 -0400

    Refactor for using .env

--- src/graphql_root.test.js
+++
@@ -1,9 +0,0 @@
-import resolvers from './graphql_root';
-import queries from './queries/userQueries';
-
-describe('getUsers', () => {
-  it('gets all users', async () => {
-    const users = resolvers.getUsers();
-    await expect(users).resolves.toEqual(await queries.getUsers())
-  });
-});
--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 require('dotenv').config()
 const { ApolloServer } = require('apollo-server')
 
-
 const typeDefs = require('./schema.js');
 const resolvers = require('./resolvers');
-import { PORT, NODE_ENV } from './config';
-const port = process.env.PORT || PORT;
+const port = process.env.PORT;
 
 const server = new ApolloServer({
   typeDefs,
@@ -14,7 +12,7 @@ const server = new ApolloServer({
 
 if(process.env.NODE_ENV !== 'test') {
   server.listen({ port }).then(({ url }) => {
-    console.log(`App running on port ${port} at url ${url}. Env: ${process.env.NODE_ENV || NODE_ENV}.`)
+    console.log(`App running on port ${port} at url ${url}. Env: ${process.env.NODE_ENV}.`)
   });
 }
 
