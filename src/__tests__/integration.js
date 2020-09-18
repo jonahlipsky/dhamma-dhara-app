849489adae8eb4461ab43f6b58c87dc330ce10f6
jonahlipsky <jonahlipsky@gmail.com>
2020-09-18 10:41:52 -0400

    Add frames for tests

---
+++ src/__tests__/integration.js
@@ -0,0 +1,23 @@
+describe('Queries', () => {
+  describe('getUser', () => {
+
+  });
+  describe('getUsers', () => {
+
+  });
+});
+
+describe('Mutations', () => {
+  describe('createUser', () => {
+
+  });
+  describe('updateUser', () => {
+
+  });
+  describe('deleteUser', () => {
+
+  });
+  describe('loginUser', () => {
+
+  });
+});
